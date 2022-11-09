da908ef11384796754d89d734e2d6c8c439de81f
William_FBD <william.fredrik.bakke.dahl@nmbu.com>
2022-11-09 22:07:08 +0100

    started writing to outputfile and refining word replacment

--- forBEdrer.py
+++ forBEdrer.py
@@ -1,29 +1,27 @@
 import random as rd
 import os
 
-INPUT_FILE_NAME = "input.csv"
-OUTPUT_FILE_NAME = "output.csv"
+INPUT_FILE_NAME = "input.txt"
+OUTPUT_FILE_NAME = "output.txt"
 WORD_LIST_FILE_NAME = "wordlist.txt"
 
 wordlist = []
-error_text = ""
+error_text = []
 run = True
 
 if not os.path.isfile(INPUT_FILE_NAME):
-    raise FileExistsError("Input file does not exist")
     run = False
     error_text.append("Input file does not exist")
 if not os.path.isfile(WORD_LIST_FILE_NAME):
-    raise FileExistsError("Word list file does not exist")
     run = False
     error_text.append("Word list file does not exist")
 
 
 if run:
     with open(WORD_LIST_FILE_NAME, "r") as file:
-        data = file.read().splitlines()
+        text = file.read().splitlines()
         split_data = []
-        for line in data:
+        for line in text:
             split_data.append(line.split('='))
 
     for line in split_data:
@@ -33,17 +31,44 @@ if run:
         wordlist.append([{"replacement_words": replacement_words, "replaced_words": replaced_words}])
 
     with open(INPUT_FILE_NAME, "r") as file:
-        data = file.read().splitlines()
-        for line in data:
-            for word in line.split(" "):
-                for item in wordlist:
-                    if word in item[0]["replaced_words"]:
-                        line = line.replace(word, rd.choice(item[0]["replacement_words"]))
+        text = file.read().splitlines()
+
+    new_text = []
+
+    for line in text:
+        new_line = []
+        for word in line.split(" "):
+            replaced = False
+            for item in wordlist:
+                if word in item[0]["replaced_words"]:
+                    new_line.append(" ".join(rd.choice(item[0]["replacement_words"])))
+                    replaced = True
+            if not replaced:
+                if "be" in word:
+                    line = line.replace(word, "BE")
+                else:
+                    new_line.append(word)
+        new_text.append("\n".join(new_line))
+
+    if os.path.isfile(OUTPUT_FILE_NAME):
+        with open(OUTPUT_FILE_NAME, "w") as file:
+            for line in new_text:
+                file.write(line)
+                file.write("\n")
+    else:
+        with open(OUTPUT_FILE_NAME, "x") as file:
+            for line in new_text:
+                file.write(line)
+                file.write("\n")
+
+
 
 else:
     if os.path.isfile(OUTPUT_FILE_NAME):
         with open(OUTPUT_FILE_NAME, "w") as file:
-            file.write(error_text)
+            for error in error_text:
+                file.write(error + "\n")
     else:
         with open(OUTPUT_FILE_NAME, "x") as file:
-            file.write(error_text)
+            for error in error_text:
+                file.write(error + "\n")
